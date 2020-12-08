9c7720ce4ff35c3d3779989c9ecb30f2563fefd6
abhaysahu <42239059+abhaysahu@users.noreply.github.com>
2020-12-08 16:24:09 +0530

    change in router

---
+++ src/app/layout/component/issues/component/create-issues/create-issues.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CreateIssuesComponent } from './create-issues.component';
+
+describe('CreateIssuesComponent', () => {
+  let component: CreateIssuesComponent;
+  let fixture: ComponentFixture<CreateIssuesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CreateIssuesComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CreateIssuesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/layout/component/issues/component/create-issues/create-issues.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-issues',
+  templateUrl: './create-issues.component.html',
+  styleUrls: ['./create-issues.component.css']
+})
+export class CreateIssuesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
